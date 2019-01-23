61bee95e24972432812a26264e77f9b838f697d6
Kaeland Chatman <kaeland1@gmail.com>
2019-01-23 11:16:03 -0500

    sorta working

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,19 +11,34 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
+    this.state = {
+      transactions: [],
+      searchedTransactions: [],
+      searchedWord: ''
+    }
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(res => res.json())
+      .then(transactions => this.setState({ transactions, searchedTransactions: transactions }))
+  }
+
+  handleChange = (event) => {
+    let { searchedTransactions } = this.state 
+    this.setState({
+      searchedWord: event.target.value, 
+      searchedTransactions: transactions.filter((trans) => {
+        return trans.description.includes(event.target.value)
+      })
+    })
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search value={this.state.searchedWord} onChange={this.handleChange} />
+        <TransactionsList transactions={this.state.searchedTransactions} />
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
+  console.log("Search: ", props)
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
+        value={props.value}
+        onChange={props.onChange}
       />
       <i className="circular search link icon"></i>
     </div>
